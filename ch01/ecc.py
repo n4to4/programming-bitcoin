5c9b2b511c4eb7963aeb96b1fe856bc0479ed683
n4to4 <n4to4k@gmail.com>
2022-03-27 11:48:00 +0900

    wip

--- ch01/ecc.py
+++ ch01/ecc.py
@@ -55,8 +55,30 @@ class Point:
         self.b = b
         self.x = x
         self.y = y
+        if self.x is None and self.y is None:
+            return
         if self.y**2 != self.x**3 + a * x + b:
             raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(
+                'Points {}, {} are not on the same curve'.format(self, other)
+            )
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(self, self.x, None, self.a, self.b)
+
+        # self.x != other.x
+        if self.x != other.x and self.y != other.y:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s(self.x - x) - self.y
+            return self.__class__(self, x, y, self.a, self.b)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y \
