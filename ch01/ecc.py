139171be3ac817df3f017261977244adcf7431d4
n4to4 <n4to4k@gmail.com>
2022-03-27 11:27:33 +0900

    wip

--- ch01/ecc.py
+++ ch01/ecc.py
@@ -47,3 +47,20 @@ class FieldElement:
             raise TypeError('Divide by zero')
         num = pow(self.num, other.num, self.prime)
         return self.__class__(num, self.prime)
+
+
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return not (self == other)
